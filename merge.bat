420d2f37bbb0ef33d89e1d649dbd65d911d0c8c8
Mreren <Pc_ren@test.com>
2018-11-19 10:49:23 +0800

    init

--- merge.bat
+++ merge.bat
@@ -1,6 +1,6 @@
 
 cd /d %~dp0
 
-"E:\Program Files\ILMerge\ILMerge.exe" /ndebug /target:exe /out:dist/install.exe /log windowcode.exe /log Newtonsoft.Json.dll /log ICSharpCode.SharpZipLib.dll /log Http.dll /log InstallTools.dll /log Interop.TaskScheduler.dll  /targetplatform:v4
+"E:\Program Files\ILMerge\ILMerge.exe" /ndebug /target:exe /out:dist/DaZheng.exe /log WebApp.exe /log Newtonsoft.Json.dll /log ICSharpCode.SharpZipLib.dll /log Http.dll /log InstallTools.dll /log Interop.TaskScheduler.dll  /targetplatform:v4
 
 pause
